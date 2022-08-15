e378aaf15caadee110fd9a73e06df37de4ac44db
sraj1820 <sraj1820@gmail.com>
2022-08-15 12:34:25 -0400

    the first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Experience from './components/Experience';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Experience resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "name": "Raj Shah",
+    "role": "Fullstack Developer",
+    "phone": '647-710-6080',
+    "email": "sraj1820@gmail.com",
+    "linkedinId":"https://www.linkedin.com/in/sraj1820/",
+    "roleDescription": " I enjoy building web apps and understanding the logic behind frontend development and backend development. I love exploring and hiking as well as playing pc games. I am currently looking for any work as a junior web developer.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sraj1820/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/sraj1820",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a web development graduate that has completed a 6 month bootcamp with General Assembly. I enjoy sipping on coffee and getting to work on coding my next project. I am currently searching for a role as a junior web developer.",
+    "address":"Canada",
+    "website":"",
+    "education":[
+      {
+        "universityName":"General Assembly",
+        "specialization":"Software Engineering Immersive",
+        "graduationMonth":"May",
+        "graduationYear":"2022",
+        "description":"Had the opportunity to create multiple projects focusing on the MERN Stack. Learned multiple languages and technologies including: HTML, CSS, Javascript, React, Node, Express, MongoDB, and more."
+      },
+      {
+        "universityName":"Ted Rogers School of Management",
+        "specialization":"Business Technology Management",
+        "graduationMonth":"April",
+        "graduationYear":"2021",
+        "description":"Large focus on information systems and building new systems from scratch. Included diagramming planning and creation of mock projects. Created a final Capstone project which included the complete revamping of an existing system."
+      }
+    ],
+    "work":[
+      {
+        "companyName":"Zebra Robotics",
+        "position":"Coding/Robotics Coach",
+        "start":"March 2021 -",
+        "end":"March 2022",
+        "description":"Used critical thinking to break down problems, evaluate solutions and make decisions. Taught weekly classes to groups of 4-6 students on coding languages such as scratch, Html/CSS and JavaScript as well as robotics online and in person. Also offered friendly and efficient service to customers, handled challenging situations with ease."
+      },
+      {
+        "CompanyName":"Zebra Robotics",
+        "position":"Business Development Associate",
+        "start":"August 2021 -",
+        "end":"December 2021",
+        "description":" Enhanced sales techniques and marketing plans to strengthen business development efforts. Boosted revenue by bringing in and cementing relationships with new clients and optimizing servicing of existing customer accounts."
+      }
+    ],
+    "skillsDescription":"Languages/Technologies",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Node"
+      }
+      ,
+      {
+        "skillname":"Express"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"BlackJack Game",
+        "description":"Basic BlackJack game created using HTML, CSS and JavaScript",
+        "imgurl":"images/blackjackGame.png",
+        "link": "https://black-jack-game-htlcx0twk-sraj1820.vercel.app/",
+      },
+      {
+        "name":"Mazda App",
+        "description":"Web Portal created using html & css for the front-end and django for the back-end",
+        "imgurl":"images/mazda.png",
+        "link": " https://github.com/sraj1820/DRPROJECT",
+      },
+      {
+        "name":"HR-Trak",
+        "description":"Fullstack application using MERN stack. An HR portal to keep track of employees and departments in a company.",  
+        "imgurl":"images/hr.png",
+        "link": "https://hrtrak.herokuapp.com/employee/home",
+      }, 
+      {
+        "name":"Tic-Tac-Toe",
+        "description":"Fully Functional tictactoe game created using vanilla JS, Html, CSS.",  
+        "imgurl":"images/TTT.png",
+        "link": "https://sraj1820.github.io/Tic-Tac-Toe/",
+      },
+   
+    ],
+
+    
+  }
+  
+  export default resumeData
